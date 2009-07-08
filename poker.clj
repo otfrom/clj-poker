d0b774ef58653f719a4305a90d49faed5b524adb
Alex Scordellis <alex.scordellis@gmail.com>
2009-07-08 21:48:06 +0100

    Detect pairs

---
+++ poker.clj
@@ -0,0 +1,17 @@
+(def suits [:S :H :D :C])
+(def ranks [:A :K :Q :J :10])
+(defstruct card :suit :rank)
+
+(defn
+  #^{:test (fn []
+    (assert (false? (has-pair [])))
+    (assert (false? (has-pair [[:S :A]])))
+    (assert (true? (has-pair [[:S :A], [:H :A]])))
+    (assert (true? (has-pair [[:S :A], [:H :A], [:H :K]])))
+    (assert (false? (has-pair [[:S :A], [:H :Q], [:H :K]])))
+  )}
+  has-pair 
+  [hand]
+    (not= (count (distinct (map (fn [coll] (get coll 1)) hand))) (count hand)))
+(test #'has-pair)
+
