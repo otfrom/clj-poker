9cc34bfe480cd3759cbe583d230848fb7e16d0e4
Alex Scordellis <alex.scordellis@gmail.com>
2009-07-15 20:33:02 +0100

    Implemented shuffle

--- deck.clj
+++ deck.clj
@@ -1,3 +1,4 @@
+(import '(java.util ArrayList Collections)) 
 (def suits '(:C :D :H :S))
 (def ranks '(:A :K :Q :J :10))
 (def all-cards  (for [suit suits rank ranks] [rank suit]))
@@ -13,6 +14,25 @@
   )}
   test-all-cards [] nil)
 
+(defn #^{:test (fn []
+    (assert (= 20 (count (shuffle all-cards))))
+    (assert (every? 
+              #(= % 1) 
+              (map 
+                #(count (filter #{%1} (shuffle all-cards))) 
+                (shuffle all-cards))))
+  )}
+  shuffle 
+  "Shuffles coll using a Java ArrayList." 
+  [coll] 
+  (let [l (ArrayList. coll)] 
+    (Collections/shuffle l) 
+    (seq l))) 
+
+
+
 
 (test #'test-all-cards)
 (test #'is-card)
+(test #'shuffle)
+
