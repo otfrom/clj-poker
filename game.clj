9fea69202721bc3418a7126bf094d28a638c27c5
Alex Scordellis <alex.scordellis@gmail.com>
2009-07-20 21:27:06 +0100

    can now count ranks

--- game.clj
+++ game.clj
@@ -1,3 +1,5 @@
+(use 'clojure.set)
+
 (def *ranks* '(:A :K :Q :J :10))
 (def *suits* '(:C :D :H :S))
 
@@ -18,28 +20,55 @@
 
 (test #'shuffled-hand)
 
-(def deck (ref (shuffled-hand)))
+(def *deck* (ref (shuffled-hand)))
 
-(def players (ref []))
+(def *players* (ref []))
 
 (defn new-game [n]
   (dosync
-    (ref-set players (repeat n []))
-    (ref-set deck (shuffled-hand))
-    
-  ))
+    (ref-set *players* (repeat n []))
+    (ref-set *deck* (shuffled-hand))))
 
-(defn deal-a-card [deck]
-  (split-at 1 deck))
+(defn deal-a-card [deck]   
+ (split-at 1 deck))
 
-
+(defn give-card-to-player [player]
+  (dosync
+   (let [[next-card remaining] (deal-a-card @*deck*)]
+     (ref-set *deck* remaining)
+     (conj player (first next-card)))))    
+   
 (defn deal []
-  (let [hand (take @players @deck)]
-    (dosync
-      (ref-set deck (drop @players @deck)))
-  ))
+  (dosync 
+    (alter *players* #(map give-card-to-player %))))
+
+(defn
+  #^{:test (fn []
+            (assert (has-pair [[:10 :C] [:10 :S]]))
+            (assert (not (has-pair [[:9 :C] [:J :D]])))
+            (assert (has-pair @*deck*)))}
+    has-pair [hand]
+     (not 
+       (= (count (into #{} (map first hand))) (count hand))))
+
+(defn
+   #^{:test (fn []
+      (assert (= {:A 2 :9 1}
+                 (count-ranks [[:A :C] [:A :D] [:9 :D]]))))}
+    count-ranks [hand]
+     (reduce #(let [[rank suit] %2]
+            (if (rank %1) 
+               (assoc %1 rank (inc (rank %1)) )
+               (assoc %1 rank 1)))
+       {} hand))
 
 (new-game 5)
-(deal-a-card @deck)
 
-;@players
+(deal)
+(deal)
+
+@players
+
+(test #'has-pair)
+(test #'count-ranks)
+
